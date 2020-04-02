aa0b75f8d1788f30434ba720c75a797135f29808
Danielle Klaasen <danielleklaasen@gmail.com>
2020-04-02 20:53:09 +0200

    Update ReactiveData.vue

--- src/components/ReactiveData.vue
+++ src/components/ReactiveData.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="hello">
-    <!-- Reactive Data -->
+  <div class="reactive-data">
+    <h1>Reactive data</h1>
     {{ reactive }}
+    <p>Code example:</p>
+    <pre>
+      <code>
+&#123; &#123; reactive &#125; &#125;
+      </code>
+    </pre>
+    <br/>
+    <pre>
+      <code>
+data: () => {
+  return {
+      reactive: 'Reactive data example. This text will respond to change.'
+  };
+}
+      </code>  
+      </pre>  
   </div>
 </template>
 
