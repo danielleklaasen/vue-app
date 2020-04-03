59dc03a07e2c3a4a5a7a9388a706d00198ae5e9e
Danielle Klaasen <danielleklaasen@gmail.com>
2020-04-03 15:32:21 +0200

    Add variation to DataBinding example

--- src/components/DataBinding.vue
+++ src/components/DataBinding.vue
@@ -4,12 +4,26 @@
     <a href="https://vuejs.org/v2/guide/syntax.html#Attributes">Docs</a>
     <br/>
     <br/>
-    <img :alt="imageAlt" src="../assets/logo.png" />
+    <img :alt="imageAlt" src="../assets/kittens.jpg" />
+    &nbsp;
+    <img v-bind="{
+        alt: imageAlt
+      }" src="../assets/kittens.jpg" />
     <p>Code example:</p>
     <pre>
       <code>
 &lt;!-- can also be written as v-bind:alt --&gt;
-&lt;img id="mainImg" :alt="imageAlt" src="../assets/logo.png" /&gt;
+
+&lt;img id="mainImg" :alt="imageAlt" src="../assets/kittens.jpg" /&gt;
+
+
+&lt;!-- or passed to v-bind as an object --&gt;
+
+&lt;img 
+  id="mainImg"
+  v-bind="{ alt: imageAlt }" 
+  src="../assets/kittens.jpg" 
+/&gt;
       </code>
     </pre>
     <br/>
@@ -30,7 +44,7 @@ export default {
   name: "DataBinding",
   data: () => {
     return {
-      imageAlt: "Logo"
+      imageAlt: "Kitten"
     };
   }
 };
